e71bc400295bdf3c9b09476c0a1d9e735434f38d
Karesz <81579751+Kareszrk@users.noreply.github.com>
2022-12-06 13:29:42 +0100

    Create file_example.js

---
+++ file_example.js
@@ -0,0 +1,15 @@
+// We delete the unnecessary file from our disk.
+async function removeUncessaryTempFile(){
+  // We delete all the uploaded unecessary files
+  let userTempFiles = JSON.parse(JSON.stringify(req.files));
+  // Now we break up the JSON data, and get the "key" of each selected file.
+  // This is required to let us delete EVERY temp created files the user sent us.
+  // JSON data mapper source: https://www.zhenghao.io/posts/object-vs-map
+  for (const key in userTempFiles) {
+    if (Object.prototype.hasOwnProperty.call(userTempFiles, key)) {
+      fs.unlink(userTempFiles[key].tempFilePath, (err) => {
+        if(err) console.log(err);
+      });
+    }
+  }
+}
